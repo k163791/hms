ca789bba6738031c06f6b82d691c35eb77b519f6
k163791 <k163791@nu.edu.pk>
2019-08-12 05:42:17 +0500

    initial commit

---
+++ includes/navigation.php
@@ -0,0 +1,44 @@
+<html>
+<body>
+    <!-- Navigation Panel -->
+    <div class="home">
+        <nav class="navbar navbar-expand-md navbar-dark bg-dark">
+            <a href="/hms" class="navbar-brand">
+                <i class="fa fa-plus-square"></i>
+            </a>
+            <span class="navbar-text" style="color:#fff;font-size:30px;">
+                HMS
+            </span>
+            <button
+                class="navbar-toggler"
+                type="button"
+                data-toggle="collapse"
+                data-target="#navbarResponsive"
+                >
+                <span class="navbar-toggler-icon"></span>
+                </button>
+            <div class="collapse navbar-collapse" id="navbarResponsive">
+                <ul class="navbar-nav ml-auto">
+                <li class="nav-item dropdown">
+                    <a data-toggle="dropdown" data-target="dropdown_target" href="#" class="dropdown-toggle nav-link">
+                        POS
+                        <span class="caret"></span>
+                    </a>
+                    <div class="dropdown-menu dropdown-menu-right"
+                        aria-labelledby="dropdown_target"
+                        role="menu"
+                        >
+                        <a href="create_invoices.php" class="dropdown-item">Create Invoice</a>
+                        <a href="" class="dropdown-item">View Invoices</a>
+                    </div>
+                </li>
+                <li class="nav-item">
+                    <a href="/hms" class="nav-link">Back to Home</a>
+                </li>
+                </ul>
+            </div>
+            </nav>
+    </div>
+
+</body>
+</html>
